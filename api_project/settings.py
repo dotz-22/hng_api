31d310a2056b105cd766854e8ee7c282e193d809
dotz-22 <babarindesheriff@gmail.com>
2025-01-29 21:44:28 +0100

    deployment issues

--- api_project/settings.py
+++ api_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-godipw)m)@twg2&z*zx2nobvy3i#*a#uyk2tgxpf6c7optb+s%'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://hng-api-dy5y.onrender.com']
 
 
 # Application definition
