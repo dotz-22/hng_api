821d0791e79acef839686c52ee559ed621d843ed
dotz-22 <babarindesheriff@gmail.com>
2025-01-29 17:04:01 +0100

    first commit

--- api_app/views.py
+++ api_app/views.py
@@ -7,7 +7,7 @@ def Json_details_View(request):
     data = {
         "email": "babarindesheriff@gmail.com.com",
         "current_datetime": now().isoformat(),
-        "github_url": "https://github.com/yourusername/your-repo"
+        "github_url": "https://github.com/dotz-22/hng_api.git"
     }
     return JsonResponse(data)
 
