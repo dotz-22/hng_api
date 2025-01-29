8377c1cb03453344bc88ca4e1dceb2981a059e89
dotz-22 <babarindesheriff@gmail.com>
2025-01-29 16:50:55 +0100

    commit 1

---
+++ api_app/views.py
@@ -0,0 +1,13 @@
+from django.http import JsonResponse
+from django.utils.timezone import now
+
+# Create your views here.
+
+def Json_details_View(request):
+    data = {
+        "email": "babarindesheriff@gmail.com.com",
+        "current_datetime": now().isoformat(),
+        "github_url": "https://github.com/yourusername/your-repo"
+    }
+    return JsonResponse(data)
+
