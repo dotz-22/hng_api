1f4fe388c1ccf5b940065a880a04cd5061fe719f
dotz-22 <babarindesheriff@gmail.com>
2025-01-30 10:56:08 +0100

    deployment issues

--- api_project/settings.py
+++ api_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://hng-api-dy5y.onrender.com']
+ALLOWED_HOSTS = ['hng-api-dy5y.onrender.com']
 
 
 # Application definition
